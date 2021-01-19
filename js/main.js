c61e163012ed02063d2e999993422e8827427e8d
SQflex <32853595+Sqflex@users.noreply.github.com>
2021-01-19 05:30:12 +0300

    global: made perfect one portfolio (adaptive included)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 new WOW().init();
 var mySwiper = new Swiper('.swiper-container', {
     speed: 400,
-    spaceBetween: 100
+    spaceBetween: 100,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
 });
